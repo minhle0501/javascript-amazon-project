2d1f809e408685d506be52369271aa5d0976b6f9
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-16 16:19:28 +0700

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,9 @@
-console.log('hello');
+
 //tạo mảng vì mảng đại diện cho danh sách
 //dùng object đẻ đại diện cho mỗi sản phẩm
 //object cho phép ta nhóm lại nhiều giá khác nhau 
 
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090,
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095,
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799,
-}];
+
 //1.tạo ra biên global
 let productsHTML = '';
 //dùng forEach để loop qua từng object trên(lưu object dưới parem product chay function)
