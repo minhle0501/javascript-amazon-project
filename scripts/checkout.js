ba2d6dc8df32e264399cf446b7e58c9e72d4c169
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-17 17:35:48 +0700

    delete product cart when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,8 @@ cart.forEach((cartItem) => {
   //console.log(matchingProduct);
 
     cartSummaryHTML += `
-    <div class="cart-item-container ">
+    <div class="cart-item-container 
+     js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -107,8 +108,7 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click', () => {
     const {productId} = link.dataset;
     removeFromCart(productId);
-    console.log(cart);
-     
-      
+    const container = document.querySelector(`.js-cart-item-container-${productId}`
+    ).remove();
   });
 })
