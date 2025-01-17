6d8b3cd4aa7a007094d15fa633a56f16099e258f
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-17 09:06:09 +0700

    add message added va them selector quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -8,12 +8,12 @@
 let productsHTML = '';
 //dùng forEach để loop qua từng object trên(lưu object dưới parem product chay function)
 products.forEach((product) => {
-    //và chuyển nó qua html bằng cách lưu ở biến html
-    //để lưu đc nhiều giá trị ta dùng object syntax
-    
-    //2.dùng mẫu tích lũy (accumulator Pattern) ta lặp qua mảng
-    //và mỗi lần đó chúng ta thêm vào kết quả +=
-    productsHTML += `
+  //và chuyển nó qua html bằng cách lưu ở biến html
+  //để lưu đc nhiều giá trị ta dùng object syntax
+
+  //2.dùng mẫu tích lũy (accumulator Pattern) ta lặp qua mảng
+  //và mỗi lần đó chúng ta thêm vào kết quả +=
+  productsHTML += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -53,7 +53,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -66,55 +66,93 @@ products.forEach((product) => {
     `;
 })
 
- //kết hơp các html này và put nó lên web
- document.querySelector('.js-products-grid')
- .innerHTML = productsHTML;
+//kết hơp các html này và put nó lên web
+document.querySelector('.js-products-grid')
+  .innerHTML = productsHTML;
+
+//Chúng ta sẽ sử dụng một đối tượng để lưu các id thời gian chờ.
+// Lý do chúng ta sử dụng một đối tượng là vì mỗi sản phẩm
+// sẽ có timeoutId riêng. Vì vậy, một đối tượng cho phép chúng ta
+//Lưu nhiều ID thời gian chờ cho các sản phẩm khác nhau.
+// For example:
+// {
+//   'product-id1': 2,
+//   'product-id2': 5,
+//   ...
+// }
+// 2 và 5 là các id được trả về khi chúng ta gọi setTimeout).
+const addedMessageTimeouts = {};
+
 // làm việc với Add to Cart lưu dưới file cart sau đó
 //đặt tên data attribute
 //  data-product-name = "${product.name}
- document.querySelectorAll('.js-add-to-cart')
- .forEach((button) => {
-    button.addEventListener('click',() => {
-        //dùng data set để chuyển dữ liệu (chuyển thừ kebab to camel)
-        const productId = button.dataset.productId;
-       /*  trong 1 sản phẩm thương mại thì có nhiều sản phẩm có cùng tên với nhau nhưng khác hãng 
-        để giải quyết vấn đề này ta dùng id
-        const productName = button.dataset.productName; */
-        //loop dữ liệu qua cart để không bị trùng sản phẩm mà chỉ thêm quantity
-        //lưu biến item dưới dạng biến để t dùng nó sau này và cho nó underfield
-        let matchingItem;
-        //dùng for each
-        cart.forEach((item) => {
-            //nếu như cart trùng 
-            if(productId === item.productId){
-                matchingItem = item;
-            }
-        });
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      //dùng data set để chuyển dữ liệu (chuyển thừ kebab to camel)
+      //short cut destructuring
+      const { productId } = button.dataset;
+      /*  trong 1 sản phẩm thương mại thì có nhiều sản phẩm có cùng tên với nhau nhưng khác hãng 
+       để giải quyết vấn đề này ta dùng id
+       const productName = button.dataset.productName; */
+      //loop dữ liệu qua cart để không bị trùng sản phẩm mà chỉ thêm quantity
+      //lưu biến item dưới dạng biến để t dùng nó sau này và cho nó underfield
+      let matchingItem;
+      //dùng for each
+      cart.forEach((item) => {
+        //nếu như cart trùng 
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
 
-        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
-        const quantity = Number(quantitySelector.value);
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantity = Number(quantitySelector.value);
 
-        if(matchingItem){
-            matchingItem.quantity += quantity;
-        }else{
+      if (matchingItem) {
+        matchingItem.quantity += quantity;
+      } else {
         //push dữ liệu vào cart
         cart.push({
-            productId: productId,
-            quantity: quantity,
+          productId,
+          quantity,
         });
-        }
-        
-        //cho giá trị của cart hiện tại bằng 0
-        let cartQuantity = 0;
+      }
 
-        cart.forEach((item) => {
-            //tính số lượng product thêm vao cart
-            cartQuantity += item.quantity;
-        })
-        
-        document.querySelector('.js-cart-quantity')
+      //cho giá trị của cart hiện tại bằng 0
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        //tính số lượng product thêm vao cart
+        cartQuantity += item.quantity;
+      })
+
+      document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
 
-        
+      //message added div 
+      const addedMessage =
+      document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      addedMessage.classList.add('added-to-cart-visible');
+      setTimeout(() => {
+        console.log(addedMessage.classList.remove('added-to-cart-visible'));
+      }, 2000);
+      //  Kiểm tra xem có thời gian chờ trước đó cho việc này hay không
+      //  Nếu có, chúng ta nên ngăn chặn nó.
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+
+      // Save the timeoutId for this product
+      // so we can stop it later if we need to.
+      addedMessageTimeouts[productId] = timeoutId;
     });
- });
+  });
