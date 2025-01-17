ce9364ea6f1c673a893ac2781500dcb44d1182d9
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-17 21:41:31 +0700

    14h

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -41,9 +41,12 @@ cart.forEach((cartItem) => {
                   <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span> 
-                  <span class="update-quantity-link link-primary">
+                  <span class="update-quantity-link link-primary 
+                  js-update-link data-product-id="${matchingProduct.id}"">
                     Update
                   </span>
+                  <input class="quantity-input">
+            <span class="save-quantity-link link-primary">Save</span>
                   <span class="delete-quantity-link link-primary 
                   js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
@@ -115,8 +118,19 @@ document.querySelectorAll('.js-delete-link')
 })
 function UpdateCartQuantity(){
   const cartQuantity = calculateCartQuantity();
-  
+
 document.querySelector('.js-return-to-home-link')
 .innerHTML = `${cartQuantity} items`;
 }
 UpdateCartQuantity();
+
+document.querySelectorAll('.js-update-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.add('is-editing-quantity');
+    });
+  });
