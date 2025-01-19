e8cd2ed2cadedd411270dd60b25af6e875a095f9
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-19 15:04:38 +0700

    save data & generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -6,9 +6,11 @@ if(!cart){
   cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity : 2,
+    deliveryOptionId : '1'
   }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity : 1,
+    deliveryOptionId : '2'
   }];
 }
 //lưu dữ liệu vào card
@@ -41,6 +43,7 @@ export function addToCart(productId){
          cart.push({
            productId,
            quantity,
+           deliveryOptionId : '1'
          });
        }
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -7,6 +7,19 @@ import { products } from "../data/products.js";
 
 import {formatCurrency} from './utils/money.js';
 
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+
+//import hàm dayjs với default export (cập nhật thời gian thực)
+import  dayjs  from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
+import { deliveryOptions } from "../data/deliveryOptions.js";
+hello();
+const today = dayjs();
+//thêm ngày nhận hàng
+const deliveryDate = today.add(7, 'ngày');
+console.log(deliveryDate.format('dddd,DD-MM-YYYY'));
+
+
 let cartSummaryHTML = ``; 
  
 cart.forEach((cartItem) => {
@@ -21,12 +34,31 @@ cart.forEach((cartItem) => {
   });
     
   //console.log(matchingProduct);
+  //delivery date cập nhật ngày
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+  deliveryOptions.forEach((option) => {
+    if(option.id === deliveryOptionId){
+      deliveryOption = option;
+    }
+  });
+
+  //sau khi kiểm tra đúng với cartItem.deliveryOptionId;
+  //dùng lại để cập nhật ngày giao hàng của function tùy chọn giao hàng
+  const today = dayjs();
+
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays
+    ,'days');
+    const dateString = deliveryDate.format('dddd, DD-MM-YYYY' );
+
 
     cartSummaryHTML += `
     <div class="cart-item-container 
      js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
-              Delivery date: Tuesday, June 21
+              Ngày giao hàng : ${dateString}
             </div>
 
             <div class="cart-item-details-grid">
@@ -42,25 +74,25 @@ cart.forEach((cartItem) => {
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
+                    số lượng: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                   </span> 
                  
                    <span class="update-quantity-link link-primary 
                    js-update-link" data-product-id="${matchingProduct.id}">
-                    Update
+                    Cập nhật
                   </span>
                 
                 <input class="quantity-input js-quantity-input-${matchingProduct.id}">  
 
                   <span class="save-quantity-link link-primary js-save-link"
                   data-product-id="${matchingProduct.id}">
-                  Save
+                  Lưu
                  </span>
 
 
                   <span class="delete-quantity-link link-primary 
                   js-delete-link" data-product-id="${matchingProduct.id}">
-                    Delete
+                    Xóa
                   </span>
 
                 </div>
@@ -70,57 +102,48 @@ cart.forEach((cartItem) => {
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                ${deliveryOptionHTML(matchingProduct, cartItem)}
               </div>
             </div>
           </div>
     `
 });
+//hàm tùy chọn giao hàng
+function deliveryOptionHTML(matchingProduct, cartItem){
+  let html = ``;
+  deliveryOptions.forEach((deliveryOption) =>{
+    const today = dayjs();
+
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays
+    ,'days');
+    const dateString = deliveryDate.format(
+      'dddd, DD-MM-YYYY'
+    );
+    const priceString = deliveryOption
+    .priceCents === 0 
+    ? 'Miễn phí vận chuyển' : `$${formatCurrency(deliveryOption.priceCents)} - Phí `;
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+    html +=`<div class="delivery-option">
+                  <input type="radio"
+                  ${isChecked ? 'checked' : ''}
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      ${dateString}
+                    </div>
+                    <div class="delivery-option-price">
+                      ${priceString} 
+                    </div>
+                  </div>
+                </div>`;                          
+  });
+  return html;
+}
+
 //console.log(cartSummaryHTML);
 document.querySelector('.js-order-summary')
 .innerHTML = cartSummaryHTML;
@@ -136,6 +159,8 @@ document.querySelectorAll('.js-delete-link')
     UpdateCartQuantity();
   });
 })
+
+//hàm cập nhật số lương
 function UpdateCartQuantity(){
   const cartQuantity = calculateCartQuantity();
 
