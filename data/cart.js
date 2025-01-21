79265687fced3885739d587c3552f038ac705837
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-21 10:49:13 +0700

    test and update cart ,ordersummary

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,22 @@
 //load du lieu
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-//nếu k có cart để save thì cho nó default
-if(!cart){
-  cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity : 2,
-    deliveryOptionId : '1'
-  }, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity : 1,
-    deliveryOptionId : '2'
-  }];
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
 }
 //lưu dữ liệu vào card
 function saveToStorage(){
--- scripts/checkout/OrderSummary.js
+++ scripts/checkout/OrderSummary.js
@@ -48,6 +48,7 @@ import { cart,
   
       cartSummaryHTML += `
       <div class="cart-item-container 
+      js-cart-item-container
        js-cart-item-container-${matchingProduct.id}">
               <div class="delivery-date">
                 Ngày giao hàng : ${dateString}
@@ -64,7 +65,8 @@ import { cart,
                   <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                   </div>
-                  <div class="product-quantity">
+                  <div class="product-quantity 
+                   js-product-quantity-${matchingProduct.id}">
                     <span>
                       số lượng: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
                     </span> 
@@ -83,7 +85,7 @@ import { cart,
   
   
                     <span class="delete-quantity-link link-primary 
-                    js-delete-link" data-product-id="${matchingProduct.id}">
+                    js-delete-link  js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                       Xóa
                     </span>
   
