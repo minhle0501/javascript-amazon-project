19d80cc2beaea4afede1ab3bf06f4dc5f075677f
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-01-22 14:59:26 +0700

    create oop cart version

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderCheckoutHeader()
 renderOrderSummary();
